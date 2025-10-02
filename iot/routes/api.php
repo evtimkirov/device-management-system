2f8a0d33ed7d3443b42c4fdbe4cee2aa1ea256af
Evtim Kirov <evtimkirov@gmail.com>
2025-10-02 09:09:13 +0300

    User routes

--- iot/routes/api.php
+++ iot/routes/api.php
@@ -1,11 +1,20 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
+    # Login
     Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
-    Route::get('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
-});
 
+    Route::middleware('auth:sanctum')->group(function () {
+        # Auth options
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::get('/me', [AuthController::class, 'me']);
+
+        # Users
+        Route::post('/users', [UserController::class, 'store']);
+        Route::delete('/users/{id}', [UserController::class, 'destroy']);
+    });
+});
